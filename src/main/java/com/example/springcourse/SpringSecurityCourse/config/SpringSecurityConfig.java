b3e391775432d5dc142a9a884d5c9381943345a6
deprim <72526094+deprim@users.noreply.github.com>
2025-09-14 21:13:36 +0300

    логшщ

--- src/main/java/com/example/springcourse/SpringSecurityCourse/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springcourse/SpringSecurityCourse/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -53,12 +54,6 @@ public class SpringSecurityConfig {
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
     }
-
-
-
-
-
-
 
 
 }
